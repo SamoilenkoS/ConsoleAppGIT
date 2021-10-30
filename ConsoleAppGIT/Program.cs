dfdd97dddfac97641e310b477be7185788423475
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2021-10-30 12:15:07 +0300

    Move & refactor code functions

--- ConsoleAppGIT/Program.cs
+++ ConsoleAppGIT/Program.cs
@@ -1,16 +1,14 @@
-﻿using System;
+﻿using GitLibrary;
+using System;
 
 namespace ConsoleAppGIT
 {
     class Program
     {
-        static void Task3()
-        {
-            Console.Write("Task implemented");
-        }
-
         static void Main(string[] args)
         {
+            //Ctrl + Shift + Space  => show InteliSence params
+            Console.WriteLine(HomeWork1.Task4(5, 6, 7));
         }
     }
 }
---
+++ GitLibrary/HomeWork1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GitLibrary
+{
+    public class HomeWork1
+    {
+        public static double Task1(int A, int B)
+        {
+            return (5.0 * A + Math.Pow(B, 2)) / (B - A);
+        }
+
+        public static void Task2(ref string A, ref string B)
+        {
+            string temp = A;
+            A = B;
+            B = temp;
+        }
+
+        public static (int division, int reminder) Task3(int A, int B)
+        {
+            return (A / B, A % B);
+        }
+
+        public static double Task4(double A, double B, double C)
+        {
+            return (C - B) / A;
+        }
+
+        public static (double A, double B) Task5(double X1, double Y1, double X2, double Y2)
+        {
+            double A = (Y1 - Y2) / (X1 - X2);
+
+            double B = Y2 - A * X2;
+
+            return (A, B);
+        }
+
+    }
+}
